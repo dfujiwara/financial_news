259b02f0926773b72ef028869eae6eef15520f33
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-12-08 23:00:57 -0800

    Initial rss test code (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello')
+import Parser = require('rss-parser')
+
+let parser = new Parser()
+let run = async () => {
+  let feed = await parser.parseURL("https://www.reddit.com/.rss")
+  console.log(feed.title)
+
+  feed.items.forEach(item => {
+    console.log(item.title + ":" + item.link)
+  })
+}
+run()
