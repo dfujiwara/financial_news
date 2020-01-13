78ae4707ec46a938227b3e839971dea14f7dd8b7
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-12 23:37:10 -0800

    Add dispatcher code (#4)

---
+++ src/dispatcher.ts
@@ -0,0 +1,12 @@
+import { rssURLList } from './rssConfig'
+import { PubSub } from '@google-cloud/pubsub'
+
+export function financialNews() {
+    const pubsub = new PubSub()
+    const topic = pubsub.topic('financial_new_dispatcher')
+    const promises = rssURLList.map(url => {
+        const json = { url }
+        return topic.publishJSON({ json })
+    })
+    return Promise.all(promises)
+}
