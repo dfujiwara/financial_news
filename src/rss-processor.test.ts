9ba35da135679a3c7441efb1405277022ff397a5
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-19 22:57:46 -0800

    Store news rss data into Firestore

--- src/rss-processor.test.ts
+++ src/rss-processor.test.ts
@@ -1,10 +1,11 @@
-import { NewsRssParser } from './rss-processor'
+import { NewsRssParser, NewsRssStorage, NewsData } from './rss-processor'
 import * as Parser from 'rss-parser'
 import { LanguageServiceClient } from '@google-cloud/language'
 
 jest.mock('rss-parser')
 jest.mock('@google-cloud/language')
 describe('For rss processing', () => {
+    let storage: NewsRssStorage
     beforeEach(() => {
         Parser.prototype.parseURL = jest.fn(
             (
@@ -27,14 +28,18 @@ describe('For rss processing', () => {
         LanguageServiceClient.prototype.analyzeSentiment = jest.fn((document: {[index: string]: any}) => {
             return Promise.resolve([{documentSentiment: {score: 10, magnitude: 11}}])
         })
+        storage = {
+            storeData: jest.fn()
+        }
     })
     test('sentiment results are returned correctly', async () => {
-        const parser = new NewsRssParser()
-        const sentimentResults = await parser.parse('https://news.com')
+        const parser = new NewsRssParser(storage)
+        const newsData = await parser.parse('https://news.com')
         expect(Parser).toHaveBeenCalledTimes(1)
         expect(LanguageServiceClient).toHaveBeenCalledTimes(1)
-        expect(sentimentResults.length).toEqual(1)
-        expect(sentimentResults[0].score).toEqual(10)
-        expect(sentimentResults[0].magnitude).toEqual(11)
+        expect(storage.storeData).toHaveBeenCalledTimes(1)
+        expect(newsData.length).toEqual(1)
+        expect(newsData[0].sentimentResult.score).toEqual(10)
+        expect(newsData[0].sentimentResult.magnitude).toEqual(11)
     })
 })
--- src/rss-processor.ts
+++ src/rss-processor.ts
@@ -1,24 +1,50 @@
 import * as Parser from 'rss-parser'
 import { LanguageServiceClient } from '@google-cloud/language'
+import { Firestore, WriteResult } from '@google-cloud/firestore'
 
-export interface SentimentAnalysisResult {
+interface SentimentAnalysisResult {
   score: number,
   magnitude: number
 }
+export interface NewsData {
+  title: string,
+  link: string,
+  sentimentResult: {score: number, magnitude: number},
+  date: string
+}
+
+export interface NewsRssStorage {
+  storeData(data: NewsData): Promise<void>
+}
 
 export class NewsRssParser {
-  async parse(url: string): Promise<SentimentAnalysisResult[]> {
+  private readonly storage: NewsRssStorage
+  constructor(storage: NewsRssStorage = new Storage()) {
+    this.storage = storage
+  }
+
+  async parse(url: string): Promise<NewsData[]> {
     let feed = await this.fetchRSS(url)
-    const promises = feed.items.map(item => {
-      return this.analyzeSentiment(`${item.title}:${item.contentSnippet}`)
+    const promises = feed.items.map(async item => {
+      const sentimentResult = await this.analyzeSentiment(`${item.title}:${item.contentSnippet}`)
+      const data = {
+        title: item.title,
+        sentimentResult,
+        link: item.link,
+        date: item.pubDate
+      }
+      await this.storage.storeData(data)
+      return data
     })
     return Promise.all(promises)
   }
+
   private async fetchRSS(url: string) {
     let parser = new Parser()
     let feed = await parser.parseURL(url)
     return feed
   }
+
   private async analyzeSentiment(content: string): Promise<SentimentAnalysisResult> {
     const client = new LanguageServiceClient()
     const document = {
@@ -28,5 +54,20 @@ export class NewsRssParser {
     const [result] = await client.analyzeSentiment({ document: document })
     const sentiment = result.documentSentiment
     return {score: sentiment.score, magnitude: sentiment.magnitude}
+  }
+}
+
+class Storage implements NewsRssStorage  {
+  private readonly firestore: Firestore
+  private readonly collectionName = 'financial-rss-feed'
+
+  constructor() {
+    this.firestore = new Firestore()
+  }
+
+  async storeData(data: NewsData): Promise<void> {
+    const docRef = this.firestore.collection(this.collectionName).doc(data.title)
+    await docRef.set(data)
+    return Promise.resolve()
   }
 }
