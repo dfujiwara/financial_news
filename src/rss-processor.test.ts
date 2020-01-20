b18cff724ff3d75a5a1f2d153f6b86e534314956
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-19 21:57:02 -0800

    Initial jest test

--- src/rss-processor.test.ts
+++ src/rss-processor.test.ts
@@ -1,5 +1,22 @@
-describe('', () => {
-    test('', () => {
+import { processNewsRSS, Message } from './rss-processor'
+import * as Parser from 'rss-parser'
+import * as Language from '@google-cloud/language'
 
+jest.mock('rss-parser')
+jest.mock('@google-cloud/language')
+describe('For rss processing', () => {
+    describe('if the URL parameter is missing from the pubsub data', () => {
+        let message: Message
+        beforeEach(() => {
+            const dataPayload = {json: {abc: 'ninja'}}
+            const dataPayloadJSON = JSON.stringify(dataPayload)
+            message = {data: Buffer.from(dataPayloadJSON).toString('base64')}
+        })
+        test('an error is thrown', () => {
+            expect.assertions(1)
+            processNewsRSS(message).catch(e => {
+                expect(e).toMatch('invalid payload: {"json":{"abc":"ninja"}')
+            })
+        })
     })
 })
