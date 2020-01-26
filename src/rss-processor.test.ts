0c0365e7562b46f020052f692621aaf0a2b13d86
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-25 23:34:56 -0800

    Refactor by using closures instead of interfaces

--- src/rss-processor.test.ts
+++ src/rss-processor.test.ts
@@ -1,35 +1,29 @@
-import { NewsRssParser, NewsRssStorage, SentimentAnalyzer, RSSFetcher } from './rss-processor'
+import { NewsRssParser, StorageClosure, FetchClosure, AnalyzeClosure } from './rss-processor'
 
 describe('For rss processing', () => {
-    let fetcher: RSSFetcher
-    let storage: NewsRssStorage
-    let analyzer: SentimentAnalyzer
+    let fetch: FetchClosure
+    let store: StorageClosure
+    let analyze: AnalyzeClosure
     beforeEach(() => {
-        fetcher = {
-          fetch: jest.fn(() =>
-            Promise.resolve([
-              {
-                title: "title",
-                link: "https://link.news.com",
-                pubDate: "",
-                contentSnipped: "snippet"
-              }
-            ])
-          )
-        }
-        storage = {
-            storeData: jest.fn()
-        }
-        analyzer = {
-            analyze: jest.fn(() => Promise.resolve({score: 10, magnitude: 11}))
-        }
+        fetch = jest.fn(() =>
+          Promise.resolve([
+            {
+              title: "title",
+              link: "https://link.news.com",
+              pubDate: "",
+              contentSnipped: "snippet"
+            }
+          ])
+        )
+        store = jest.fn()
+        analyze = jest.fn(() => Promise.resolve({score: 10, magnitude: 11}))
     })
     test('sentiment results are returned correctly', async () => {
-        const parser = new NewsRssParser(fetcher, analyzer, storage)
+        const parser = new NewsRssParser(fetch, analyze, store)
         const newsData = await parser.parse('https://news.com')
-        expect(fetcher.fetch).toHaveBeenCalledTimes(1)
-        expect(analyzer.analyze).toHaveBeenCalledTimes(1)
-        expect(storage.storeData).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(analyze).toHaveBeenCalledTimes(1)
+        expect(store).toHaveBeenCalledTimes(1)
         expect(newsData.length).toEqual(1)
         expect(newsData[0].sentimentResult.score).toEqual(10)
         expect(newsData[0].sentimentResult.magnitude).toEqual(11)
