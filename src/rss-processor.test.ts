77933e92541a9e91e702fbfe54615c7eafea98f3
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-25 20:16:02 -0800

    Refactor rss-prcessor so that there is more separation of concern
    The idea is to use more of dependency injection instead of mocking
    complicated objects

--- src/rss-processor.test.ts
+++ src/rss-processor.test.ts
@@ -1,59 +1,40 @@
-import { processNewsRSS, Message } from './rss-processor'
+import { NewsRssParser } from './rss-processor'
 import * as Parser from 'rss-parser'
 import { LanguageServiceClient } from '@google-cloud/language'
 
 jest.mock('rss-parser')
 jest.mock('@google-cloud/language')
 describe('For rss processing', () => {
-    describe('if the URL parameter is missing from the pubsub data', () => {
-        let message: Message
-        beforeEach(() => {
-            const dataPayload = {json: {abc: 'ninja'}}
-            const dataPayloadJSON = JSON.stringify(dataPayload)
-            message = {data: Buffer.from(dataPayloadJSON).toString('base64')}
-        })
-        test('an error is thrown', () => {
-            expect.assertions(1)
-            processNewsRSS(message).catch(e => {
-                expect(e).toMatch('invalid payload: {"json":{"abc":"ninja"}')
-            })
+    beforeEach(() => {
+        Parser.prototype.parseURL = jest.fn(
+            (
+            feedUrl: string,
+            callback?: (err: Error, feed: Parser.Output) => void,
+            redirectCount?: number
+            ) => {
+                return Promise.resolve({
+                    items: [
+                        {
+                        title: "title",
+                        link: "https://link.news.com",
+                        pubDate: "",
+                        contentSnipped: "snippet"
+                        }
+                    ]
+                    })
+            }
+        )
+        LanguageServiceClient.prototype.analyzeSentiment = jest.fn((document: {[index: string]: any}) => {
+            return Promise.resolve([{documentSentiment: {score: 10, magnitude: 11}}])
         })
     })
-    describe('with valid URL parameter in the pubsub data', () => {
-        let message: Message
-        beforeEach(() => {
-            const dataPayload = {json: {url: 'https://news.com'}}
-            const dataPayloadJSON = JSON.stringify(dataPayload)
-            message = {data: Buffer.from(dataPayloadJSON).toString('base64')}
-            Parser.prototype.parseURL = jest.fn(
-              (
-                feedUrl: string,
-                callback?: (err: Error, feed: Parser.Output) => void,
-                redirectCount?: number
-              ) => {
-                  return Promise.resolve({
-                       items: [
-                         {
-                           title: "title",
-                           link: "https://link.news.com",
-                           pubDate: "",
-                           contentSnipped: "snippet"
-                         }
-                       ]
-                     })
-                }
-            )
-            LanguageServiceClient.prototype.analyzeSentiment = jest.fn((document: {[index: string]: any}) => {
-                return Promise.resolve([{documentSentiment: {score: 10, magnitude: 11}}])
-            })
-        })
-        test('sentiment results are returned correctly', async () => {
-            const sentimentResults = await processNewsRSS(message)
-            expect(Parser).toHaveBeenCalledTimes(1)
-            expect(LanguageServiceClient).toHaveBeenCalledTimes(1)
-            expect(sentimentResults.length).toEqual(1)
-            expect(sentimentResults[0].score).toEqual(10)
-            expect(sentimentResults[0].magnitude).toEqual(11)
-        })
+    test('sentiment results are returned correctly', async () => {
+        const parser = new NewsRssParser()
+        const sentimentResults = await parser.parse('https://news.com')
+        expect(Parser).toHaveBeenCalledTimes(1)
+        expect(LanguageServiceClient).toHaveBeenCalledTimes(1)
+        expect(sentimentResults.length).toEqual(1)
+        expect(sentimentResults[0].score).toEqual(10)
+        expect(sentimentResults[0].magnitude).toEqual(11)
     })
 })
--- src/rss-processor.ts
+++ src/rss-processor.ts
@@ -1,42 +1,32 @@
 import * as Parser from 'rss-parser'
 import { LanguageServiceClient } from '@google-cloud/language'
 
-export interface Message {
-  data: string
-}
-
-interface SentimentAnalysisResult {
+export interface SentimentAnalysisResult {
   score: number,
   magnitude: number
 }
 
-export async function processNewsRSS(pubSubMessage: Message): Promise<SentimentAnalysisResult[]> {
-  let parser = new Parser()
-  const decodedData = Buffer.from(pubSubMessage.data, 'base64').toString()
-  const { json: { url: url} } = JSON.parse(decodedData)
-  if (!url) {
-    const error = `invalid payload: ${decodedData}`
-    console.error(error)
-    throw error
+export class NewsRssParser {
+  async parse(url: string): Promise<SentimentAnalysisResult[]> {
+    let feed = await this.fetchRSS(url)
+    const promises = feed.items.map(item => {
+      return this.analyzeSentiment(`${item.title}:${item.contentSnippet}`)
+    })
+    return Promise.all(promises)
   }
-  let feed = await parser.parseURL(url)
-  const promises = feed.items.map(item => {
-    console.log(item.title + ":" + item.link)
-    return analyzeSentiment(`${item.title}:${item.contentSnippet}`)
-  })
-  return Promise.all(promises)
-}
-
-async function analyzeSentiment(content: string): Promise<SentimentAnalysisResult> {
-  const client = new LanguageServiceClient()
-  const document = {
-    content,
-    type: "PLAIN_TEXT"
+  private async fetchRSS(url: string) {
+    let parser = new Parser()
+    let feed = await parser.parseURL(url)
+    return feed
   }
-  const [result] = await client.analyzeSentiment({ document: document })
-  const sentiment = result.documentSentiment
-  console.log(`Text: ${content}`)
-  console.log(`Sentiment score: ${sentiment.score}`)
-  console.log(`Sentiment magnitude: ${sentiment.magnitude}`)
-  return {score: sentiment.score, magnitude: sentiment.magnitude}
+  private async analyzeSentiment(content: string): Promise<SentimentAnalysisResult> {
+    const client = new LanguageServiceClient()
+    const document = {
+      content,
+      type: "PLAIN_TEXT"
+    }
+    const [result] = await client.analyzeSentiment({ document: document })
+    const sentiment = result.documentSentiment
+    return {score: sentiment.score, magnitude: sentiment.magnitude}
+  }
 }
