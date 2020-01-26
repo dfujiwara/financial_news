48241240c520b13cac21513980365243aa41548d
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-25 20:33:51 -0800

    Refactor rss-prcessor so that there is more separation of concern (#26)
    The idea is to use more of dependency injection instead of mocking
    complicated objects

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-export { processNewsRSS } from './rss-processor'
+import { NewsRssParser, SentimentAnalysisResult } from './rss-processor'
 export { dispatchFinancialNewsProcessing } from './dispatcher'
+
+export interface Message {
+  data: string
+}
+
+export async function processNewsRSS(pubSubMessage: Message): Promise<SentimentAnalysisResult[]> {
+  const decodedData = Buffer.from(pubSubMessage.data, 'base64').toString()
+  const { json: { url: url} } = JSON.parse(decodedData)
+  if (!url) {
+    const error = `invalid payload: ${decodedData}`
+    console.error(error)
+    throw error
+  }
+  const parser = new NewsRssParser()
+  return parser.parse(url)
+}
