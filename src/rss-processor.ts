ac519e62a05a8a8b9722c9528adc26b6438771b7
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-01-30 00:01:25 -0800

    Swap the implementation to use the functional approach (#34)

--- src/rss-processor.ts
+++ src/rss-processor.ts
@@ -1,59 +1,23 @@
-import * as Parser from 'rss-parser'
-import { LanguageServiceClient } from '@google-cloud/language'
-import { Firestore } from '@google-cloud/firestore'
-import { NewsData, AnalyzedNewsData, SentimentAnalysisResult} from './data-models'
+import { fetch } from './rss-fetcher'
+import { analyze } from './sentiment-analysis'
+import { store } from './rss-storage'
 
-export type FetchClosure = (url: string) => Promise<NewsData[]>
-export type AnalyzeClosure = (content: string) => Promise<SentimentAnalysisResult>
-export type StorageClosure = (data: AnalyzedNewsData) => Promise<AnalyzedNewsData>
-
-export class NewsRssParser {
-  private readonly fetch: FetchClosure
-  private readonly analyze: AnalyzeClosure
-  private readonly store: StorageClosure
-
-  constructor(fetchClosure: FetchClosure = fetch, analyzeClosure: AnalyzeClosure = analyze, storageClosure: StorageClosure = storeData)  {
-    this.fetch = fetchClosure
-    this.analyze = analyzeClosure
-    this.store = storageClosure
-  }
-
-  async parse(url: string): Promise<AnalyzedNewsData[]> {
-    let items = await this.fetch(url)
-    const promises = items.map(async item => {
-      const sentimentResult = await this.analyze(`${item.title}:${item.contentSnippet}`)
-      return this.store({ ...item, sentimentResult })
-    })
-    return Promise.all(promises)
-  }
+export interface Message {
+  data: string
 }
 
-async function fetch(url: string): Promise<NewsData[]> {
-  const parser = new Parser()
-  const results = await parser.parseURL(url)
-  return results.items.map((item) => ({
-      title: item.title || 'undefined',
-      link: item.link || 'undefined',
-      date: item.pubDate || 'undefined',
-      contentSnippet: item.contentSnippet || 'undefined'
-  }))
-}
-
-async function analyze(content: string): Promise<SentimentAnalysisResult> {
-  const document = {
-    content,
-    type: "PLAIN_TEXT"
+export async function processNewsRSS(pubSubMessage: Message) {
+  const decodedData = Buffer.from(pubSubMessage.data, 'base64').toString()
+  const { json: { url: url} } = JSON.parse(decodedData)
+  if (!url) {
+    const error = `invalid payload: ${decodedData}`
+    console.error(error)
+    throw error
   }
-  const client = new LanguageServiceClient()
-  const [result] = await client.analyzeSentiment({ document: document })
-  const sentiment = result.documentSentiment
-  return {score: sentiment.score, magnitude: sentiment.magnitude}
-}
-
-async function storeData(data: AnalyzedNewsData): Promise<AnalyzedNewsData> {
-    const firestore = new Firestore()
-    const collectionName = 'financial-rss-feed'
-    const docRef = firestore.collection(collectionName).doc(data.title)
-    await docRef.set(data)
-    return data
+  const newsDataList = await fetch(url)
+  const promises = newsDataList.map(async data => {
+     const analyzedData = await analyze(data)
+     return store(analyzedData)
+  })
+  return Promise.all(promises)
 }
