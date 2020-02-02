ea9210b6ecdcc551f65dcf6aa0584a6ea558726e
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-02-01 22:28:28 -0800

    Use the hashed article link for the document title
    for google cloud firestore

--- src/rss-storage.ts
+++ src/rss-storage.ts
@@ -1,10 +1,14 @@
 import { Firestore } from '@google-cloud/firestore'
 import { AnalyzedNewsData } from './data-models'
+import * as crypto from 'crypto'
+
+const collectionName = 'financial-rss-feed'
 
 export async function store(data: AnalyzedNewsData): Promise<AnalyzedNewsData> {
     const firestore = new Firestore()
-    const collectionName = 'financial-rss-feed'
-    const docRef = firestore.collection(collectionName).doc(data.title)
+    const hashableContent = `${data.link}`
+    const hash = crypto.createHash('sha1').update(hashableContent).digest('hex')
+    const docRef = firestore.collection(collectionName).doc(hash)
     await docRef.set(data)
     return data
 }
