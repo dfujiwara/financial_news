4b48239428741a2817ad9b2647558142cce04fd3
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-02-02 00:27:42 -0800

    Add the rss-storage method to retrieve data from database

--- src/rss-storage.ts
+++ src/rss-storage.ts
@@ -12,3 +12,24 @@ export async function store(data: AnalyzedNewsData): Promise<AnalyzedNewsData> {
     await docRef.set(data)
     return data
 }
+
+export async function get(fromDate: Date = new Date(), forDays: number): Promise<AnalyzedNewsData[]> {
+    const millisecondsInDay = 1000 * 60 * 60 * 24
+    const toDate = new Date(fromDate.getTime() - (millisecondsInDay * forDays))
+    const firestore = new Firestore()
+    const collection = firestore.collection(collectionName)
+    const snapShot = await collection
+        .where('date', '<=', fromDate)
+        .where('date', '>', toDate)
+        .get()
+    return snapShot.docs.map(doc => {
+        const data = doc.data() as AnalyzedNewsData
+        return {
+            title: data.title,
+            link: data.link,
+            date: data.date,
+            contentSnippet: data.contentSnippet,
+            sentimentResult: data.sentimentResult
+        }
+    })
+}
