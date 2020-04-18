4c2a944e8b2e504b6a730566d86b7269f1857092
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-04-18 14:07:58 -0700

    Expand the rss list to include other sources

--- src/rss-config.ts
+++ src/rss-config.ts
@@ -1,1 +1,5 @@
-export const rssURLList = ['https://www.ft.com/?format=rss']
+export const rssURLList = [
+    'https://www.ft.com/?format=rss',
+    'http://feeds.marketwatch.com/marketwatch/topstories/',
+    'https://www.cnbc.com/id/20910258/device/rss/rss.html',
+]
